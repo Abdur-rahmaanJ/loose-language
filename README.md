6ea1758dd540117f501d284a0ed78b85a816803d
El saidi <el@Weiss>
2017-03-10 00:06:23 +0100

    Update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # loose-language
 Project on creating a new programming language that is doing almost nothing.
+
+But are you sure ???????? ;-)
+Author Email : vertolive13@gmail.com
--- interprtor.py
+++ interprtor.py
@@ -4,31 +4,40 @@ import sys
 
 PLUS, MINUS, EGAL = '+', '-', '='
 
-sfon_dictionnay = {'print': print}
+sfon_dictionnay = {}
 tvar_dictionnay = {}
 
 def fileToList(path_program):
     if path_program.find('.loose') == len(path_program)-1:
         raise TypeError('Wrong type file')
-    
+    # open the file and store in a buffer
     f = open(str(path_program), 'r')
     str_buf = f.read()
     f.close
-
+    
+    # get ride of the line break and split
     str_buf = str_buf.replace('\n','')
     list_buf = str_buf.split(';')
+    
+    # get ride of spaces in each element of list_buf
+    for i in range(0, len(list_buf)-1):
+        list_buf[i] = list_buf[i].replace(' ','')
 
-    for i in range(0,len(list_buf)-1): 
+    i = 0
+    # algo to suppress empty elements
+    # a master piece : yay...
+    while i < len(list_buf): 
         if list_buf[i] == '':
             del list_buf[i]
+            if i != 0:
+                i = i - 1
+        else:
+            i = i + 1
+        
     return list_buf
 
-def interprtor(list_program):
-    for line in list_program:
-        line =  line.replace(' ','')
-        
-        
-        print line
+def interprtor(list_program): 
+    print list_program
 
 def main():
     try:
